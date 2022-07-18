02584f26f6854bf1a4b962356d6844a5f4052130
luca-bossio <lbossio2003@gmail.com>
2022-07-18 17:50:35 -0300

    cambio

--- src/components/ItemCount/index.jsx
+++ src/components/ItemCount/index.jsx
@@ -1,13 +1,23 @@
 import  "./itemCount.css";
-
-import React from "react";
+import React, { useState } from "react";
 
 export const ItemCount = () => {
+    const [count , setcount] = useState(1)
+
+    const decrease = () => {
+        setcount (count - 1)
+        
+    }
+
+    const increase = () => {
+        setcount (count + 1)
+    }
+
     return ( 
     <div className="count">
-        <button>-</button>
+        <button onClick={decrease}>-</button>
         <span>2</span>
-        <button>+</button>
+        <button onClick={increase}>+</button>
         <div>
             <button>Agregar al carrito</button>
         </div>
--- src/components/ItemListContainer/index.jsx
+++ src/components/ItemListContainer/index.jsx
@@ -1,10 +1,16 @@
+
 import React from "react";
+
 import Title from "../title";
+
 import ItemCount from "../ItemCount";
 
 export const ItemListContainer = ({ texto }) => {
     return (
+        <>
         <Title greeting={texto} />
+        < ItemCount />
+        </>
     )
 }
 
