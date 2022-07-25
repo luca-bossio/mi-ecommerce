ccf8a9c4d2424df8e558174abcb9987c71659f73
luca-bossio <lbossio2003@gmail.com>
2022-07-25 17:06:43 -0300

    cambios

--- src/components/Item/index.jsx
+++ src/components/Item/index.jsx
@@ -1,7 +1,7 @@
 import "./item.css";
 import React from "react";
 
-const Item = (info) => {
+const Item = ({info}) => {
     return (
         <a href="" className="books">
             <img src={info.image} alt="DW" />
--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -4,34 +4,34 @@ import "./navbar.css"
 
 
 export const Navbar = () => {
-    return (
-        <div className="grid-area">
-        <nav class="navbar navbar-expand-lg ">
-    <div class="container">
-    <a class="navbar-brand" href="#">
-        <h1>LO-MAS colchones</h1>
-      </a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
-        aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <div class="navbar-nav lista ">
-          <a class="nav-link active menu" aria-current="page" >Inicio</a>
-          <a class="nav-link active menu" >Marcas </a>
-          <a class="nav-link active menu" >Contacto</a>
-          <a class="nav-link active menu" >Redes</a>
-          <a class="nav-link active menu" >Tienda <Cartwidget /> </a>
+  return (
+    <div className="grid-area">
+      <nav className="navbar navbar-expand-lg">
+        <div className="container">
+          <a className="navbar-brand" href="#">
+            <h1>LO-MAS colchones</h1>
+          </a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div className="navbar-nav lista ">
+              <a className="nav-link active menu" aria-current="page" >Inicio</a>
+              <a className="nav-link active menu" >Marcas </a>
+              <a className="nav-link active menu" >Contacto</a>
+              <a className="nav-link active menu" >Redes</a>
+              <a className="nav-link active menu" >Tienda <Cartwidget /> </a>
+            </div>
+          </div>
         </div>
-      </div>
+      </nav>
     </div>
-  </nav>
-  </div>
-
-        
 
 
-    );
+
+
+  );
 }
 
 export default Navbar;
