be91fcd5faa17d5467ad3027d399cc02c2074e90
luca-bossio <lbossio2003@gmail.com>
2022-08-01 19:41:53 -0300

    CAMBIOS

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,23 @@ import './App.css';
 
 import Cart from './components/Cart';
 
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-
-import Navbar from "./components/Navbar/index"
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import ItemdetailContainer from './components/ItemDetailContainer';
+
+import ItemListContainer from './components/ItemListContainer';
 
 function App() {
   return (
     <>
 
       <BrowserRouter>
-        <NavBar >
           <Routes>
             <Route path='/' element={<ItemListContainer />} />
             <Route path='/categoria/:categoriaId' element={<ItemListContainer />} />
             <Route path='/cart' element={<Cart />} />
             <Route path='/detalle/:detalleId' element={<ItemDetailContainer />} />
           </Routes>
-        </NavBar>
       </BrowserRouter>
     </>
   );
--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Cartwidget from "../Cartwidget"
 import "./navbar.css"
+import ItemListContainer from "../ItemListContainer"
 import { NavLink } from 'react-router-dom'
 
 
