a1118fbb4cf5dcfe85026693628d3572e0df3696
luca-bossio <lbossio2003@gmail.com>
2022-08-31 20:21:16 -0300

    cambios

--- src/components/Item/index.jsx
+++ src/components/Item/index.jsx
@@ -1,16 +1,20 @@
 import "./item.css";
 import React  from "react";
 import { Link } from 'react-router-dom';
+import "../Css/main.css"
 
 
 const Item = ({info}) => {
    
-
+<h1 className="titulo">Pulse el colchon que quiere:</h1>
     return (
-        <Link to={`/detalle/${info.id}`} className="Colchones">
-            <img src={info.image} alt="DW" />
-            <p>{info.title}</p>
+   
+        <>
+        <Link to={`/detalle/${info.id}`} className="Colchones"> 
+            <h2 className="tituloColchon">{info.title}</h2>
+            <img src={info.image} alt="DW" width="80%"/>
         </Link>
+        </>
     )
 }
 
